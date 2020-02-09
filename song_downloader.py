3fb783f5036bc9d4cb6b6dc05d40f7494bad09b0
Aakash Singh <46787056+aakash714@users.noreply.github.com>
2020-02-09 14:18:31 +0530

    added error hanelling

--- song_downloader.py
+++ song_downloader.py
@@ -1,18 +1,19 @@
 #credits: erazer
 
-from tqdm import tqdm
-import requests
-import time
 import json
+import time
 
-#constants
-current_milli_time = lambda: int(round(time.time() * 1000))
+import requests
+from tqdm import tqdm
+
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:72.0) Gecko/20100101 Firefox/72.0',
     'Accept': '*/*',
     'Referer': 'http://musicpleer.cloud/'
-    }
+}
+
+response_items = 10 #number of response items
 
 
 #search for the song
@@ -25,20 +26,31 @@ def search_song(_srh):
         'jsoncallback':'jQuery1111019191608358321144_1580929162911',
         'qry':_srh,#song name goes here
         'format':'json',
-        'mh':'10' ,#number of response items
+        'mh':response_items,
         'where':'mpl'
     }
 
-    search_resp = requests.get(search_url,headers=headers,params=search_params)
+    try:
+        search_resp = requests.get(search_url,headers=headers,params=search_params)
+    
+    except requests.exceptions.Timeout:
+        search_song(srh)
+
+    except requests.exceptions.ConnectionError:
+        print('\tconnection ERROR\n ')
+        exit(1)
     
     if search_resp.text == '':
-        print ('no results found\n\n')
+        print ('no results found\n')
+        
         mainfunc()
+    
     else:
         search_json = json.loads(search_resp.text[43:-1])
         print('SONGS FOUND')
-        return get_song(search_json)
-  
+        
+        get_song(search_json)
+
 
 #get the song data from database 
 def get_song(_search_json):
@@ -46,24 +58,31 @@ def get_song(_search_json):
     song_url = 'https://databrainz.com/api/data_api_new.cgi'
     
     i = 0
-    while i <= 10:
+    while i <= response_items:
         
         song_params = {
             'jsoncallback': 'jQuery1111019191608358321144_1580929162911',
-            'id':_search_json['results'][i]['url'] ,#song url goes here
+            'id':_search_json['results'][i]['url'] , #song url goes here
             'r': 'mpl',
             'format': 'json',
-            '_':current_milli_time()
+            '_':int(round(time.time() * 1000))
         }
 
         print('FETCHING SONG INFO...')
-        song_resp = requests.get(song_url,headers=headers,params=song_params)
+        try:
+            song_resp = requests.get(song_url,headers=headers,params=song_params)
+        
+        except requests.exceptions.ConnectionError:
+            print('\tconnection ERROR\n ')
+            exit(1)
+        
         song_json = json.loads(song_resp.text[43:-1])
 
         if song_json['song']['returncode'] != '200':
+            print('\tnot found')
             i += 1
             continue
-
+        
         else:
             print('\n---RESULT---')
             print('title: {}'.format(song_json['song']['title']))
@@ -71,49 +90,65 @@ def get_song(_search_json):
             print('album: {}'.format(song_json['song']['album']))
             print('release date: {}'.format(song_json['song']['date']))
             
-            a = ' '
-            while a != 'y' or a != 'n':
-                
-                a = input('Do you want to to download this song [{}] [y/n]? '.format(str(round((int(song_json['song']['size'])/1000000),2))+' MB'))
-                
-                if a.lower() == 'y':
-                    
-                    #download the song
-                    print('\nDOWNLOADING...\n')
-                    file_name = song_json['song']['artist']+" - "+song_json['song']['title']+".mp3"
-                    
-                    response = requests.get(song_json['song']['url'],stream=True)
-                    
-                    #progress bar stuff
-                    total_size = int(response.headers.get('content-length', 0))
-                    block_size = 1024 #1 Kibibyte
-                    t=tqdm(total=total_size, unit='iB', unit_scale=True)
+            download_song(song_json)
 
-                    with open(file_name,'wb') as file:
-                        for data in response.iter_content(block_size):
-                            t.update(len(data)) #update the progress bar
-                            file.write(data)
-                    t.close()
-                    print('DOWNLOAD SUCESSFUL\n')
-                    break
 
-                elif a.lower() == 'n':
-                    break
-                else:
-                    continue
+#download the song
+def download_song(_song_json):
 
-            mainfunc()
+    a = ' '
+    while a != 'y' or a != 'n':
+        
+        a = input(
+            'Do you want to to download this song [{}] [y/n]? '.format(
+                str(round((int(_song_json['song']['size'])/1000000),2))+' MB'
+                )
+            )
+        
+        if a.lower() == 'y':
+            
+            print('\nDOWNLOADING...\n')
+            try:
+                file_resp = requests.get(_song_json['song']['url'],stream=True)
+
+            except requests.exceptions.ConnectionError:
+                print('\tconnection ERROR\n ')
+                exit(1)
+            
+            file_name = _song_json['song']['artist']+" - "+_song_json['song']['title']+".mp3"
+            
+            total_size = int(file_resp.headers.get('content-length', 0))
+            t = tqdm(total=total_size, unit='iB', unit_scale=True)
+
+            with open(file_name,'wb') as file:
+                for data in file_resp.iter_content(1024):
+                    t.update(len(data)) #update the progress bar
+                    file.write(data)
+            
+            t.close()
+            print('DOWNLOAD SUCESSFUL\n')
+            break
+        
+        elif a.lower() == 'n':
+            break
+        
+        else:
+            continue
+        
+    mainfunc()
 
 
 #main function
 def mainfunc():
-    print("\n---Songs downloader---\ntype 'exit' to terminate the program")
+    
+    print("\n===|Songs downloader|===\ntype 'exit' to terminate the program")
     
     srh = input('Name of the song: ')
-    if srh.lower() != 'exit':
-        search_song(srh)
+    if srh.lower() == 'exit':
+        exit(0)
     else:
-        pass
+        search_song(srh)
+
 
 #main function called
 mainfunc()
